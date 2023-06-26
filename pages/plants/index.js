a2f4516f75e9ddb6fcf764f8774c55c4143dcddc
Marcus Bohmann <mbohmann@gmx.de>
2023-06-26 14:37:23 +0200

    delete unnecessary code

--- pages/plants/index.js
+++ pages/plants/index.js
@@ -1,11 +1,5 @@
 import PlantsList from "@/components/PlantsList";
 
 export default function Plants() {
-  return (
-    <>
-      <div>
-        <PlantsList />
-      </div>
-    </>
-  );
+  return <PlantsList />;
 }
