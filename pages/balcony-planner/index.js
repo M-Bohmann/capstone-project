2d252a94ba3ecebad26fb8338f07f6075a532b3a
Marcus Bohmann <mbohmann@gmx.de>
2023-06-26 16:21:35 +0200

    add balcony plant planner page

---
+++ pages/balcony-planner/index.js
@@ -0,0 +1,12 @@
+import PlantsList from "@/components/PlantsList";
+
+export default function BalconyPlantsPlanner() {
+  return (
+    <>
+      <h1>Balkon Pflanzenplaner</h1>
+      <button>Filter</button>
+      <h2>Pflanzenempfehlung</h2>
+      <PlantsList />
+    </>
+  );
+}
