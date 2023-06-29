fe308969fb5e07ed98a3ba8abf827ade16b79567
Marcus Bohmann <mbohmann@gmx.de>
2023-06-29 10:42:01 +0200

    add usestore and basic styling

--- pages/balcony-planner/index.js
+++ pages/balcony-planner/index.js
@@ -1,12 +1,28 @@
 import FilteredPlantsList from "@/components/FilteredPlantsList";
+import Link from "next/link";
+import { useStore } from "../_app";
+import styled from "styled-components";
 
 export default function BalconyPlantsPlanner() {
+  const plantList = useStore((state) => state.plantList);
+
   return (
     <>
-      <h1>Balkon Pflanzenplaner</h1>
-      <button>Filter</button>
+      <StyledHeading>Balkon Pflanzenplaner</StyledHeading>
+      <StyledLink href="/balcony-planner/filter">
+        <button>Filter</button>
+      </StyledLink>
       <h2>Pflanzenempfehlung</h2>
-      <FilteredPlantsList />
+      <FilteredPlantsList plants={plantList} />
     </>
   );
 }
+
+const StyledLink = styled(Link)`
+  display: flex;
+  justify-content: center;
+`;
+
+const StyledHeading = styled.h1`
+  text-align: center;
+`;
