5f3f664d299b34573afb961bdcac97a8c69f5173
Marcus Bohmann <mbohmann@gmx.de>
2023-06-29 10:24:29 +0200

    add create from zustand

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,13 @@
+import { plants } from "@/lib/plants";
 import GlobalStyle from "../styles";
+import { create } from "zustand";
+
+const useStore = create((set) => ({
+  plantList: plants,
+  setPlantList: (plantList) => set(() => ({ plantList: plantList })),
+}));
+
+export { useStore };
 
 export default function App({ Component, pageProps }) {
   return (
