bb08fe4f880eea536648f1ddafa39764f7e6bf93
Marcus Bohmann <mbohmann@gmx.de>
2023-06-30 08:52:00 +0200

    add MyBalconyPlants component to the page

--- pages/balcony-planner/index.js
+++ pages/balcony-planner/index.js
@@ -2,6 +2,7 @@ import FilteredPlantsList from "@/components/FilteredPlantsList";
 import Link from "next/link";
 import { useStore } from "../_app";
 import styled from "styled-components";
+import MyBalconyPlants from "@/components/MyBalconyPlants";
 
 export default function BalconyPlantsPlanner() {
   const plantList = useStore((state) => state.plantList);
@@ -9,6 +10,8 @@ export default function BalconyPlantsPlanner() {
   return (
     <>
       <StyledHeading>Balkon Pflanzenplaner</StyledHeading>
+      <h2>Mein Balkon</h2>
+      <MyBalconyPlants />
       <StyledLink href="/balcony-planner/filter">
         <button>Filter</button>
       </StyledLink>
