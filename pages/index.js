2e921441f015eae642e8c38d69a7a46f0f2cf412
Marcus Bohmann <mbohmann@gmx.de>
2023-07-04 14:58:28 +0200

    use navbar instead of Links

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,3 @@
-import Link from "next/link";
-
 export default function HomePage() {
-  return (
-    <ul>
-      <li>
-        <Link href="/plants">Übersicht insektenfreundlicher Pflanzen</Link>
-      </li>
-      <li>
-        <Link href="/balcony-planner">Zum Balkon Pflanzenplaner</Link>
-      </li>
-    </ul>
-  );
+  return <h1>Balkon mit insektenfreundlichen Pflanzen gestalten </h1>;
 }
