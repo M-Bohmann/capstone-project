90b0cd6d5c3c816fc4701d449eea0d2a8759d3d0
Marcus Bohmann <mbohmann@gmx.de>
2023-07-11 13:53:18 +0200

    delete return

--- pages/api/plants/index.js
+++ pages/api/plants/index.js
@@ -6,8 +6,8 @@ export default async function handler(request, response) {
 
   if (request.method === "GET") {
     const plants = await Plant.find();
-    return response.status(200).json(plants);
+    response.status(200).json(plants);
   } else {
-    return response.status(405).json({ message: "Method not allowed" });
+    response.status(405).json({ message: "Method not allowed" });
   }
 }
