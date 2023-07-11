8806122766a7c39e68572eeeffe562dffa39f909
Marcus Bohmann <mbohmann@gmx.de>
2023-07-11 13:50:33 +0200

    get data from db

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,15 @@
-import { plants } from "@/lib/plants";
 import GlobalStyle from "@/styles";
 import useLocalStorageState from "use-local-storage-state";
 import { uid } from "uid";
 import NavBar from "@/components/NavBar";
 import { SWRConfig } from "swr";
+import useSWR from "swr";
 
 const fetcher = (url) => fetch(url).then((response) => response.json());
 
 export default function App({ Component, pageProps }) {
+  const { data: plants } = useSWR("/api/plants");
+
   const [filteredPlants, setFilteredPlants] = useLocalStorageState(
     "filteredPlants",
     {
