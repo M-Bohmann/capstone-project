33d0ca9568b0c25a7ec5520e2753be9c397a9df0
Marcus Bohmann <mbohmann@gmx.de>
2023-07-12 15:35:18 +0200

    add note to the balconyPlantSchema

--- db/models/BalconyPlant.js
+++ db/models/BalconyPlant.js
@@ -19,6 +19,7 @@ const balconyPlantSchema = new Schema({
       "https://images.unsplash.com/photo-1530224417158-b0f5c5615418?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80",
   },
   isUserPlant: Boolean,
+  note: String,
 });
 
 const BalconyPlant =
