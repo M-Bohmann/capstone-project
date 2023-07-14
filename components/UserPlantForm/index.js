4dda8ddc8c6ad6c141bc2b1ca87150a5fd055eee
Marcus Bohmann <mbohmann@gmx.de>
2023-07-14 13:20:19 +0200

    add defaultValues to the UserPlantForm

--- components/UserPlantForm/index.js
+++ components/UserPlantForm/index.js
@@ -3,25 +3,44 @@ import Link from "next/link";
 import { StyledFieldset } from "./UserPlantForm.styled";
 import { months } from "@/lib/plants";
 
-export default function UserPlantForm({ onSubmit }) {
+export default function UserPlantForm({ onSubmit, defaultValue }) {
   return (
     <>
       <h1>Deine Pflanze</h1>
       <StyledForm onSubmit={onSubmit}>
         <label htmlFor="name">Name:</label>
-        <input id="name" name="name" type="text" required />
+        <input
+          id="name"
+          name="name"
+          type="text"
+          required
+          defaultValue={defaultValue?.name}
+        />
         <label htmlFor="botanicalName">Botanischer Name:</label>
-        <input id="botanicalName" name="botanicalName" type="text" />
+        <input
+          id="botanicalName"
+          name="botanicalName"
+          type="text"
+          defaultValue={defaultValue?.botanicalName}
+        />
         <StyledFieldset>
           <legend>Eigenschaften</legend>
           <label htmlFor="usageType">Pflanzenart:</label>
-          <select id="usageType" name="usageType">
+          <select
+            id="usageType"
+            name="usageType"
+            defaultValue={defaultValue?.usageType}
+          >
             <option></option>
             <option>Nutzpflanze</option>
             <option>Zierpflanze</option>
           </select>
           <label htmlFor="lightRequirements">Standort:</label>
-          <select id="lightRequirements" name="lightRequirements">
+          <select
+            id="lightRequirements"
+            name="lightRequirements"
+            defaultValue={defaultValue?.lightRequirements}
+          >
             <option></option>
             <option>Sonnig</option>
             <option>Halbschatten</option>
@@ -29,29 +48,42 @@ export default function UserPlantForm({ onSubmit }) {
           </select>
           <label htmlFor="growthHeight">Wuchshöhe:</label>
           <div>
-            <input id="growthHeight" name="growthHeight" type="number" />
+            <input
+              id="growthHeight"
+              name="growthHeight"
+              type="number"
+              defaultValue={defaultValue?.growthHeight}
+            />
             <span> cm</span>
           </div>
           <label htmlFor="hardy">Winterhart:</label>
-          <select id="hardy" name="hardy">
+          <select id="hardy" name="hardy" defaultValue={defaultValue?.hardy}>
             <option></option>
             <option>ja</option>
             <option>nein</option>
           </select>
           <label htmlFor="bloomStart">Blütezeit:</label>
-          <select id="bloomStart" name="bloomStart">
+          <select
+            id="bloomStart"
+            name="bloomStart"
+            defaultValue={defaultValue?.bloomStart}
+          >
             {months.map((month) => (
               <option key={month}>{month}</option>
             ))}
           </select>
           <span>bis</span>
-          <select id="bloomEnd" name="bloomEnd">
+          <select
+            id="bloomEnd"
+            name="bloomEnd"
+            defaultValue={defaultValue?.bloomEnd}
+          >
             {months.map((month) => (
               <option key={month}>{month}</option>
             ))}
           </select>
           <label htmlFor="nectar">Nektargehalt:</label>
-          <select id="nectar" name="nectar">
+          <select id="nectar" name="nectar" defaultValue={defaultValue?.nectar}>
             <option></option>
             <option>1</option>
             <option>2</option>
@@ -59,7 +91,7 @@ export default function UserPlantForm({ onSubmit }) {
             <option>4</option>
           </select>
           <label htmlFor="pollen">Pollengehalt:</label>
-          <select id="pollen" name="pollen">
+          <select id="pollen" name="pollen" defaultValue={defaultValue?.pollen}>
             <option></option>
             <option>1</option>
             <option>2</option>
@@ -68,7 +100,12 @@ export default function UserPlantForm({ onSubmit }) {
           </select>
         </StyledFieldset>
         <label htmlFor="note">Notizen:</label>
-        <textarea id="note" name="note" type="text" />
+        <textarea
+          id="note"
+          name="note"
+          type="text"
+          defaultValue={defaultValue?.note}
+        />
         <button type="submit">Speichern</button>
         <Link href="/balcony-planner">Abbrechen</Link>
       </StyledForm>
