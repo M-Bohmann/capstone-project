3f295c23ced0fb352d562a2762cc9aca1442ae37
Marcus Bohmann <mbohmann@gmx.de>
2023-07-23 18:38:48 +0200

    remove comments

--- components/ImageUploadForm/index.js
+++ components/ImageUploadForm/index.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-// we are using useSWR to mutate the data once a file has been uploaded
 import useSWR from "swr";
 import {
   Form,
@@ -10,32 +9,27 @@ import {
 
 export default function ImageUploadForm({ onUpload }) {
   const { mutate } = useSWR("/api/images/");
-  // We define some states to give some feedback to the user what happened to our upload
   const [uploadStatus, setUploadStatus] = useState("");
   const [error, setError] = useState(undefined);
-  // a kind of 'standard' form handler
+
   async function submitImage(event) {
     event.preventDefault();
     setUploadStatus("Uploading...");
     const formData = new FormData(event.target);
-    // we use fetch to call our API and pass the form data and request method
+
     try {
       const response = await fetch("/api/upload", {
         method: "post",
         body: formData,
       });
-      // once the file is uploaded (= the promise in our api upload is resolved)
       if (response.status === 201) {
         const result = await response.json();
         const url = result.url;
-        // we call mutate to refresh our image data
         mutate();
-        // and set a successful state
         setUploadStatus("Upload complete!");
         onUpload(url);
       }
     } catch (error) {
-      // in case of error, we set the state accordingly
       setError(error);
     }
   }
