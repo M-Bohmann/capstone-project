b1701ce161bad5165a32acc77d208c0d3eece779
Marcus Bohmann <mbohmann@gmx.de>
2023-07-23 14:33:35 +0200

    add styling to the user plant form

--- components/UserPlantForm/index.js
+++ components/UserPlantForm/index.js
@@ -1,6 +1,13 @@
-import { StyledForm } from "@/pages/balcony-planner/filter";
-import Link from "next/link";
-import { StyledFieldset } from "./UserPlantForm.styled";
+import {
+  ButtonWrapper,
+  CancelLink,
+  NameSection,
+  SaveButton,
+  StyledFieldset,
+  StyledUserPlantForm,
+  TextArea,
+  TextareaWrapper,
+} from "./UserPlantForm.styled";
 import { months } from "@/lib/plants";
 import ImageUploadForm from "../ImageUploadForm";
 import { useState } from "react";
@@ -12,55 +19,67 @@ export default function UserPlantForm({ onSubmit, defaultValue }) {
     <>
       <h1>Deine Pflanze</h1>
       <ImageUploadForm onUpload={setUploadImageUrl} />
-      <StyledForm onSubmit={onSubmit}>
-        {uploadImageUrl && (
-          <input
-            type="hidden"
-            name="imgUrl"
-            id="imgUrl"
-            value={uploadImageUrl}
-          />
-        )}
-        <label htmlFor="name">Name:</label>
-        <input
-          id="name"
-          name="name"
-          type="text"
-          required
-          defaultValue={defaultValue?.name}
-        />
-        <label htmlFor="botanicalName">Botanischer Name:</label>
-        <input
-          id="botanicalName"
-          name="botanicalName"
-          type="text"
-          defaultValue={defaultValue?.botanicalName}
-        />
+      <StyledUserPlantForm onSubmit={onSubmit}>
+        <NameSection>
+          {" "}
+          {uploadImageUrl && (
+            <input
+              type="hidden"
+              name="imgUrl"
+              id="imgUrl"
+              value={uploadImageUrl}
+            />
+          )}
+          <label htmlFor="name">
+            Name:{" "}
+            <input
+              id="name"
+              name="name"
+              type="text"
+              required
+              defaultValue={defaultValue?.name}
+            />
+          </label>
+          <label htmlFor="botanicalName">
+            Botanischer Name:{" "}
+            <input
+              id="botanicalName"
+              name="botanicalName"
+              type="text"
+              defaultValue={defaultValue?.botanicalName}
+            />
+          </label>
+        </NameSection>
         <StyledFieldset>
           <legend>Eigenschaften</legend>
-          <label htmlFor="usageType">Pflanzenart:</label>
-          <select
-            id="usageType"
-            name="usageType"
-            defaultValue={defaultValue?.usageType}
-          >
-            <option></option>
-            <option>Nutzpflanze</option>
-            <option>Zierpflanze</option>
-          </select>
-          <label htmlFor="lightRequirements">Standort:</label>
-          <select
-            id="lightRequirements"
-            name="lightRequirements"
-            defaultValue={defaultValue?.lightRequirements}
-          >
-            <option></option>
-            <option>Sonnig</option>
-            <option>Halbschatten</option>
-            <option>Schatten</option>
-          </select>
-          <label htmlFor="growthHeight">Wuchshöhe:</label>
-          <div>
+          <label htmlFor="usageType">
+            Pflanzenart:{" "}
+            <select
+              id="usageType"
+              name="usageType"
+              defaultValue={defaultValue?.usageType}
+            >
+              <option></option>
+              <option>Nutzpflanze</option>
+              <option>Zierpflanze</option>
+            </select>
+          </label>
+
+          <label htmlFor="lightRequirements">
+            Standort:{" "}
+            <select
+              id="lightRequirements"
+              name="lightRequirements"
+              defaultValue={defaultValue?.lightRequirements}
+            >
+              <option></option>
+              <option>Sonne</option>
+              <option>Halbschatten</option>
+              <option>Schatten</option>
+            </select>
+          </label>
+          <label htmlFor="growthHeight">
+            Wuchshöhe:{" "}
             <input
               id="growthHeight"
               name="growthHeight"
@@ -68,60 +87,80 @@ export default function UserPlantForm({ onSubmit, defaultValue }) {
               defaultValue={defaultValue?.growthHeight}
             />
             <span> cm</span>
-          </div>
-          <label htmlFor="hardy">Winterhart:</label>
-          <select id="hardy" name="hardy" defaultValue={defaultValue?.hardy}>
-            <option></option>
-            <option>ja</option>
-            <option>nein</option>
-          </select>
-          <label htmlFor="bloomStart">Blütezeit:</label>
-          <select
-            id="bloomStart"
-            name="bloomStart"
-            defaultValue={defaultValue?.bloomStart}
-          >
-            {months.map((month) => (
-              <option key={month}>{month}</option>
-            ))}
-          </select>
-          <span>bis</span>
-          <select
-            id="bloomEnd"
-            name="bloomEnd"
-            defaultValue={defaultValue?.bloomEnd}
-          >
-            {months.map((month) => (
-              <option key={month}>{month}</option>
-            ))}
-          </select>
-          <label htmlFor="nectar">Nektargehalt:</label>
-          <select id="nectar" name="nectar" defaultValue={defaultValue?.nectar}>
-            <option></option>
-            <option value={1}>niedrig</option>
-            <option value={2}>wenig</option>
-            <option value={3}>mäßig</option>
-            <option value={4}>viel</option>
-          </select>
-          <label htmlFor="pollen">Pollengehalt:</label>
-          <select id="pollen" name="pollen" defaultValue={defaultValue?.pollen}>
-            <option></option>
-            <option value={1}>niedrig</option>
-            <option value={2}>wenig</option>
-            <option value={3}>mäßig</option>
-            <option value={4}>viel</option>
-          </select>
+          </label>
+          <label htmlFor="hardy">
+            Winterhart:{" "}
+            <select id="hardy" name="hardy" defaultValue={defaultValue?.hardy}>
+              <option></option>
+              <option>ja</option>
+              <option>nein</option>
+            </select>
+          </label>
+          <label htmlFor="bloomStart">
+            Blütezeit:{" "}
+            <select
+              id="bloomStart"
+              name="bloomStart"
+              defaultValue={defaultValue?.bloomStart}
+            >
+              {months.map((month) => (
+                <option key={month}>{month}</option>
+              ))}
+            </select>
+            <span> bis </span>
+            <select
+              id="bloomEnd"
+              name="bloomEnd"
+              defaultValue={defaultValue?.bloomEnd}
+            >
+              {months.map((month) => (
+                <option key={month}>{month}</option>
+              ))}
+            </select>
+          </label>
+          <label htmlFor="nectar">
+            Nektargehalt:{" "}
+            <select
+              id="nectar"
+              name="nectar"
+              defaultValue={defaultValue?.nectar}
+            >
+              <option></option>
+              <option value={1}>niedrig</option>
+              <option value={2}>wenig</option>
+              <option value={3}>mäßig</option>
+              <option value={4}>viel</option>
+            </select>
+          </label>
+          <label htmlFor="pollen">
+            Pollengehalt:{" "}
+            <select
+              id="pollen"
+              name="pollen"
+              defaultValue={defaultValue?.pollen}
+            >
+              <option></option>
+              <option value={1}>niedrig</option>
+              <option value={2}>wenig</option>
+              <option value={3}>mäßig</option>
+              <option value={4}>viel</option>
+            </select>
+          </label>
         </StyledFieldset>
-        <label htmlFor="note">Notizen:</label>
-        <textarea
-          id="note"
-          name="note"
-          type="text"
-          defaultValue={defaultValue?.note}
-        />
-        <button type="submit">Speichern</button>
-        <Link href="/balcony-planner">Abbrechen</Link>
-      </StyledForm>
+        <TextareaWrapper>
+          <label htmlFor="note">Notizen:</label>
+          <TextArea
+            id="note"
+            name="note"
+            type="text"
+            defaultValue={defaultValue?.note}
+          />
+        </TextareaWrapper>
+        <ButtonWrapper>
+          <CancelLink href="/balcony-planner">Abbrechen</CancelLink>
+          <SaveButton type="submit">Speichern</SaveButton>
+        </ButtonWrapper>
+      </StyledUserPlantForm>
     </>
   );
 }
